235d2ea3e44640d1e456c478b32b33815ea1c237
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-06-14 23:56:25 +0100

    s

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,5 +1,9 @@
 import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
+import { PageRouteProps } from "@/interface";
+
+// باقي الكود كما هو
+
 
 interface PageRouteProps {
   pageRoute: string;
